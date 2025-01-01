dfd48c6e6a3b5e975138b6d26ae4704ac72c5c11
anonymous-lad2 <anonymouslad540@gmail.com>
2025-01-01 18:38:18 +0530

    Expense-Tracker

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-blue-500 flex justify-center items-center">
+      <p className="font-serif font-extrabold text-2xl text-red-600 text-transform:uppercase">Expense tracker</p>
+    </div>
+  );
+}
+
+export default App;

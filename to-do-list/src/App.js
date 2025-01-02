e039d1dce558fcf8a9cb781db4609b8d66ee8b34
anonymous-lad2 <anonymouslad540@gmail.com>
2025-01-02 17:08:25 +0530

    To do List using Context Api and Redux

---
+++ to-do-list/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center bg-blue-500'>
+      <p className="text-red-700 font-extrabold text-2xl font-serif">To Do List using Context Api and Redux</p>
+    </div>
+  );
+}
+
+export default App;
